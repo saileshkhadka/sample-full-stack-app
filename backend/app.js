5d92803be35ad929d1b3bf21cf48608706b1ad15
vagrant <vagrant@manager>
2024-01-22 19:50:37 +0000

    updated docker file, and other files too along with modified github wworkflow main.yml file

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Backend!');
+  res.send('Hello from the backend!');
 });
 
 app.listen(port, () => {
   console.log(`Backend listening at http://localhost:${port}`);
 });
+
